ed095aa1a13fb30ca2e913fefac66bd581cafc76
AnyOldName3 <krizdjali@gmail.com>
2025-04-08 18:36:26 +0100

    Fix incorrect parameter and narrowing conversion warning

--- src/vsg/utils/GraphicsPipelineConfigurator.cpp
+++ src/vsg/utils/GraphicsPipelineConfigurator.cpp
@@ -566,7 +566,7 @@ void GraphicsPipelineConfigurator::_assignInheritedSets()
         {
             if (!bds.descriptorSet || !bds.descriptorSet->setLayout || !gpc.descriptorConfigurator) return;
 
-            if (gpc.shaderSet->partiallyCompatiblePipelineLayout(*bds.layout, gpc.shaderHints->defines, true, bds.firstSet))
+            if (gpc.shaderSet->partiallyCompatiblePipelineLayout(*bds.layout, gpc.shaderHints->defines, false, bds.firstSet))
             {
                 gpc.inheritedSets.insert(bds.firstSet);
             }
@@ -576,7 +576,7 @@ void GraphicsPipelineConfigurator::_assignInheritedSets()
         {
             if (!gpc.descriptorConfigurator) return;
 
-            if (gpc.shaderSet->partiallyCompatiblePipelineLayout(*bds.layout, gpc.shaderHints->defines, true, bds.firstSet + bds.descriptorSets.size() - 1))
+            if (gpc.shaderSet->partiallyCompatiblePipelineLayout(*bds.layout, gpc.shaderHints->defines, false, bds.firstSet + static_cast<uint32_t>(bds.descriptorSets.size()) - 1))
             {
                 for (size_t i = 0; i < bds.descriptorSets.size(); ++i)
                 {
@@ -587,7 +587,7 @@ void GraphicsPipelineConfigurator::_assignInheritedSets()
 
         void apply(const BindViewDescriptorSets& bvds) override
         {
-            if (!gpc.shaderSet->partiallyCompatiblePipelineLayout(*bvds.layout, gpc.shaderHints->defines, true, bvds.firstSet))
+            if (!gpc.shaderSet->partiallyCompatiblePipelineLayout(*bvds.layout, gpc.shaderHints->defines, false, bvds.firstSet))
             {
                 return;
             }
