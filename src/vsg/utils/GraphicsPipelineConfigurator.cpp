148d241d6884bb771de80ff04f4854055ce24b21
AnyOldName3 <krizdjali@gmail.com>
2025-04-09 16:06:34 +0100

    Make null checks match the new requirements

--- src/vsg/utils/GraphicsPipelineConfigurator.cpp
+++ src/vsg/utils/GraphicsPipelineConfigurator.cpp
@@ -564,7 +564,7 @@ void GraphicsPipelineConfigurator::_assignInheritedSets()
 
         void apply(const BindDescriptorSet& bds) override
         {
-            if (!bds.descriptorSet || !bds.descriptorSet->setLayout || !gpc.descriptorConfigurator) return;
+            if (!bds.layout || !gpc.descriptorConfigurator) return;
 
             if (gpc.shaderSet->partiallyCompatiblePipelineLayout(*bds.layout, gpc.shaderHints->defines, false, bds.firstSet))
             {
@@ -574,7 +574,7 @@ void GraphicsPipelineConfigurator::_assignInheritedSets()
 
         void apply(const BindDescriptorSets& bds) override
         {
-            if (!gpc.descriptorConfigurator) return;
+            if (!bds.layout || !gpc.descriptorConfigurator) return;
 
             if (gpc.shaderSet->partiallyCompatiblePipelineLayout(*bds.layout, gpc.shaderHints->defines, false, bds.firstSet + static_cast<uint32_t>(bds.descriptorSets.size()) - 1))
             {
