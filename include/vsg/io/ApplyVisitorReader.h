6482563de6805b2a054b1cdb7d6170105bd41c5c
AnyOldName3 <krizdjali@gmail.com>
2026-07-16 18:06:37 +0100

    Add ApplyReplacementVisitorReader

--- include/vsg/io/ApplyVisitorReader.h
+++ include/vsg/io/ApplyVisitorReader.h
@@ -45,4 +45,33 @@ namespace vsg
     };
     VSG_type_name(vsg::ApplyVisitorReader);
 
+    /// Class to wrap a ReaderWriter and apply a ReplacementVisitor to everything it loads
+    class VSG_DECLSPEC ApplyReplacementVisitorReader : public Inherit<ReaderWriter, ApplyReplacementVisitorReader>
+    {
+    public:
+        ApplyReplacementVisitorReader(vsg::ref_ptr<ReaderWriter> in_child, vsg::ref_ptr<ReplacementVisitor> in_visitor);
+        ApplyReplacementVisitorReader(const ApplyReplacementVisitorReader& rhs, const CopyOp& copyop = {});
+
+        vsg::ref_ptr<ReaderWriter> child;
+        vsg::ref_ptr<ReplacementVisitor> visitor;
+
+        void read(Input& input) override;
+        void write(Output& output) const override;
+
+        vsg::ref_ptr<vsg::Object> read(const vsg::Path& filename, vsg::ref_ptr<const vsg::Options> options = {}) const override;
+        vsg::ref_ptr<vsg::Object> read(std::istream& fin, vsg::ref_ptr<const vsg::Options> options = {}) const override;
+        vsg::ref_ptr<vsg::Object> read(const uint8_t* ptr, size_t size, vsg::ref_ptr<const vsg::Options> options = {}) const override;
+
+        bool write(const vsg::Object* object, const vsg::Path& filename, vsg::ref_ptr<const vsg::Options> options = {}) const override;
+        bool write(const vsg::Object* object, std::ostream& fout, vsg::ref_ptr<const vsg::Options> options = {}) const override;
+
+        bool readOptions(vsg::Options& options, vsg::CommandLine& arguments) const override;
+
+        bool getFeatures(Features& features) const override;
+
+    protected:
+        mutable std::mutex _visitorMutex;
+    };
+    VSG_type_name(vsg::ApplyReplacementVisitorReader);
+
 } // namespace vsg
